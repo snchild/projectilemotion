40d8bb0f4cc064c0bde49560dba7d8fde4dd702a
snchild <sierrachild@yahoo.com>
2023-05-11 10:44:03 -0600

    set up calling first function

--- Program.rs
+++ Program.rs
@@ -3,10 +3,13 @@ fn main() {
     println!("running main");
     //prompt the user for a ceiling height and a lauch angle (and v0?)
     //for now, let's just give them values
-    let height = 30;
+    let height = 30.0;
     let theta: f32 = 3.14159265/6.0; //note that angles should be in radians... add in pi itself another time
+    let v0 = 20.0;
+    let mut distance = -1.0; // the mut tells the program that its value will change
 
     //calculate the farthest distance if it skims the ceiling
+    distance = calculate_farthest_distance(height, theta, v0);
     //calculate flight time
     //stretch goal: plot the path if possible
 
@@ -15,22 +18,25 @@ fn main() {
     println!("theta = {theta}");
     println!("cos(th) = ");
     println!("{}", theta.cos());
+    println!("V0 = {v0}");
+    println!("distance = {distance}")
 }
 
-fn calculate_farthest_distance() {
-    println!("running calculate_farthest_distance")
+fn calculate_farthest_distance(_y: f32, _th: f32, _v: f32) -> f32 {
+    println!("running calculate_farthest_distance");
     //calculate the farthest distance here
     //return farthest distance
+    return 5.4 //replace with calculated value later
 }
 
 fn calculate_flight_time() {
-    println!("running calculate_flight_time")
+    println!("running calculate_flight_time");
     //calculate the flight time here
     //return flight time
 }
 
 fn calculate_path() {
-    println!("running calculate_path")
+    println!("running calculate_path");
     //calculate the path
     //return two lists: the x path and the y path
 }
