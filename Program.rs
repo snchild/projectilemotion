61fcf2ee063e2db8e2392a05c65098af238b5725
snchild <sierrachild@yahoo.com>
2023-05-17 16:25:29 -0600

    decided to not attempt plotting

--- Program.rs
+++ Program.rs
@@ -1,11 +1,14 @@
 use std::io; //lets us use input/output capabilities
-//use plotters::prelude::*; //lets us make plots
 
 fn main() {
     let g = 9.81; //units: m/s^2 <- acceleration due to gravity
     let mut greatest_distance = 0.0; 
     let mut deg_for_most_d = 0.0;
     let mut v0_for_most_d = 0.0;
+
+    let mut greatest_time = 0.0;
+    let mut deg_for_most_t = 0.0;
+    let mut v0_for_most_t = 0.0;
 
     loop {
         //prompt the user for a launch angle, and launch velocity
@@ -19,9 +22,7 @@ fn main() {
         let theta: f32 = degrees * 3.14159265 / 180.0; //converts the degrees into radians
         println!("Please enter a launching velocity in meters/second: ");
         let v0 = convert_to_float();
-        /*//for now, let's just give them values
-        let theta: f32 = 3.14159265/6.0; //note: angles should be in radians
-        let v0 = 20.0;*/
+
         let mut distance = -1.0; // the mut tells the program that its value will change
         let mut flight_time = -1.0; //I use the -1 value as a way to check whether the variable is reassigned later
 
@@ -34,21 +35,25 @@ fn main() {
             v0_for_most_d = v0;
             deg_for_most_d = degrees;
         }
+        
 
         //calculate flight time
         flight_time = calculate_flight_time(distance, theta, v0);
+        
+        if flight_time > greatest_time {
+            greatest_time = flight_time;
+            v0_for_most_t = v0;
+            deg_for_most_t = degrees;
+        }
         //plot the path if possible
         let x_points = calculate_x_path(theta, v0, flight_time);
         let y_points = calculate_y_path(theta, v0, flight_time, g);
 
         println!("The projectile traveled {} meters across the room in {} seconds. ", distance, flight_time);
-        //for debugging purposes: print out variables
-        //println!("xpoints = {:?}", x_points);
-        //println!("ypoints = {:?}", y_points);
-        plot_path();
     }
 
     println!("The farthest the projectile traveled was {} meters. This was when it was launched at {} meters/second, oriented {} degrees from the horizontal.", greatest_distance, v0_for_most_d, deg_for_most_d);
+    println!("The longest the projectile was in the air was {} seconds. This was when it was launched at {} meters/second, oriented {} degrees from the horizontal.", greatest_time, v0_for_most_t, deg_for_most_t);
 }
 fn convert_to_float() -> f32 {
     let mut input_text = String::new();
@@ -103,9 +108,4 @@ fn calculate_y_path(th: f32, v: f32, tf: f32, g: f32) -> [f32;21] {
         y_points[i] = v * th.sin() * t  - 0.5 * g * t * t //y = v0 * sin(theta) * t - 1/2 g t^2
     }
     return y_points
-}
-
-fn plot_path() {
-    println!("running plot_path")
-    //make a plot of the path using the two lists
 }
