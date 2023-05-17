ee03e4879020b82c99140a963fb6cc95036e54ed
snchild <sierrachild@yahoo.com>
2023-05-17 13:18:38 -0600

    incorporated a loop for the user and greatest distance

--- Program.rs
+++ Program.rs
@@ -1,39 +1,58 @@
-//include: conditionals
 use std::io; //lets us use input/output capabilities
-use plotters::prelude::*; //lets us make plots
+//use plotters::prelude::*; //lets us make plots
 
 fn main() {
-    //g is the acceleration due to gravity
-    let g = 9.81; //units: m/s^2
-    //prompt the user for a ceiling height, launch angle, and launch velocity
-    /*println!("Please enter a height in meters: ");
-    let height = convert_to_float(); //not needed unless I incorporate ceiling height
-    println!("Please enter an angle in degrees: ");
-    let degrees = convert_to_float();
-    let theta: f32 = degrees * 3.14159265 / 180.0; //converts the degrees into radians
-    println!("Please enter a launching velocity in meters/second: ");
-    let v0 = convert_to_float();*/
-    //for now, let's just give them values
-    let height = 30.0;
-    let theta: f32 = 3.14159265/6.0; //note: angles should be in radians
-    let v0 = 20.0;
-    let mut distance = -1.0; // the mut tells the program that its value will change
-    let mut flight_time = -1.0; //I use the -1 value as a way to check whether the variable is reassigned later
+    let g = 9.81; //units: m/s^2 <- acceleration due to gravity
+    let mut greatest_distance = 0.0; 
+    let mut deg_for_most_d = 0.0;
+    let mut v0_for_most_d = 0.0;
 
-    //calculate the farthest distance if there's no ceiling
-    distance = calculate_distance_no_ceiling(g, theta, v0);
+    loop {
+        //prompt the user for a ceiling height, launch angle, and launch velocity
+        println!("Please enter a height in meters (press q to quit): ");
+        let height = convert_to_float(); //not needed unless I incorporate ceiling height
 
-    //calculate flight time
-    flight_time = calculate_flight_time(distance, theta, v0);
-    //plot the path if possible
-    let x_points = calculate_x_path(theta, v0, flight_time);
-    let y_points = calculate_y_path(theta, v0, flight_time, g);
+        //check if we need to stop - happens when input isn't a float
+        if height == -1.0 {
+            break
+        }
 
-    println!("The projectile traveled {} meters across the room in {} seconds. ", distance, flight_time);
-    //for debugging purposes: print out variables
-    //println!("xpoints = {:?}", x_points);
-    //println!("ypoints = {:?}", y_points);
-    plot_path();
+        println!("Please enter an angle in degrees: ");
+        let degrees = convert_to_float();
+        let theta: f32 = degrees * 3.14159265 / 180.0; //converts the degrees into radians
+        println!("Please enter a launching velocity in meters/second: ");
+        let v0 = convert_to_float();
+        /*//for now, let's just give them values
+        let height = 30.0;
+        let theta: f32 = 3.14159265/6.0; //note: angles should be in radians
+        let v0 = 20.0;*/
+        let mut distance = -1.0; // the mut tells the program that its value will change
+        let mut flight_time = -1.0; //I use the -1 value as a way to check whether the variable is reassigned later
+
+        //calculate the farthest distance if there's no ceiling
+        distance = calculate_distance_no_ceiling(g, theta, v0);
+
+        //check if this is the farthest distance so far
+        if distance > greatest_distance {
+            greatest_distance = distance;
+            v0_for_most_d = v0;
+            deg_for_most_d = degrees;
+        }
+
+        //calculate flight time
+        flight_time = calculate_flight_time(distance, theta, v0);
+        //plot the path if possible
+        let x_points = calculate_x_path(theta, v0, flight_time);
+        let y_points = calculate_y_path(theta, v0, flight_time, g);
+
+        println!("The projectile traveled {} meters across the room in {} seconds. ", distance, flight_time);
+        //for debugging purposes: print out variables
+        //println!("xpoints = {:?}", x_points);
+        //println!("ypoints = {:?}", y_points);
+        plot_path();
+    }
+
+    println!("The farthest the projectile traveled was {} meters. This was when it was launched at {} meters/second, oriented {} degrees from the horizontal.", greatest_distance, v0_for_most_d, deg_for_most_d);
 }
 fn convert_to_float() -> f32 {
     let mut input_text = String::new();
